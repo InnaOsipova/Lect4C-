d1124f7d7e402aa5142f8f4b6c1cac3417c8abd2
Inna Osipova <rutheva_inna@mail.ru>
2022-07-11 21:54:35 +0300

    Фибоначи

---
+++ Ex004/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+
+Console.WriteLine("введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int b = 1;
+int i = 0;
+string fibo = ""+a+b;
+while (i<N-2)
+  {
+   b=a+b;
+   a=b-a;
+   fibo=fibo+b;
+   i=i+1;
+  }
+Console.WriteLine(fibo);
