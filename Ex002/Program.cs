d72e5015db9275960c94919b65ccd1295d171da8
Inna Osipova <rutheva_inna@mail.ru>
2022-07-11 20:47:50 +0300

    треугольник

---
+++ Ex002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+
+bool isTriangl (int a, int b, int c)
+{
+    if (a+b > c) 
+    {
+        if (b+c > a)
+        {
+            if (c+a > b)
+            return true;
+        }
+    }
+    return false;
+}
+
+Console.WriteLine(isTriangl(12, 8, 7));
