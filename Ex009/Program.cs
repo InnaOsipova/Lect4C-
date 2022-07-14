fb175b70ba20b2a9c77cd70ac334391366738a6d
Inna Osipova <rutheva_inna@mail.ru>
2022-07-14 20:42:15 +0300

    массив i+j

---
+++ Ex009/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 48: Задайте двумерный массив размера m на n, каждый элемент в 
+//массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+void FillArray (int m, int n)
+{
+    int [,] newarray = new int [m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newarray[i,j] = i + j;
+            Console.Write(newarray[i,j]+", ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(5,6);
