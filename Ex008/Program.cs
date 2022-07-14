b9ef7116f88c66df3262b0562300a938bc4739d7
Inna Osipova <rutheva_inna@mail.ru>
2022-07-14 20:24:54 +0300

    Создание двумерного массива

---
+++ Ex008/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 46: Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами.
+
+void FillArray (int m, int n)
+{
+    int [,] newarray = new int [m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            newarray[i,j] = new Random().Next(1, 1000);
+            Console.Write(newarray[i,j]+", ");
+            
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray(5,6);
