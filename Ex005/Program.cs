c8f649e287bce2e7d550902ab86e01252ab1c04e
Inna Osipova <rutheva_inna@mail.ru>
2022-07-14 12:59:10 +0300

    Копирование массива по эдементам

---
+++ Ex005/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 45: Напишите программу, которая будет создавать копию 
+//заданного массива с помощью поэлементного копирования.
+
+int[] CopyArray (int[] oldarray)
+{
+    int[] newArray = new int [oldarray.Length];
+    for (int i = 0; i < oldarray.Length; i++)
+    {
+        newArray[i] = oldarray[i];
+    }
+    return newArray;
+}
+
+int [] array = new int [] {1, 3, 3, 5, 7, 20, 3, 56};
+Console.WriteLine(String.Join(" | ", CopyArray( array)));
