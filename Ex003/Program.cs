0ea46746a019b5fe4b0897e27659e926f180194d
Inna Osipova <rutheva_inna@mail.ru>
2022-07-14 13:01:48 +0300

    Перевод в двоичную систему

---
+++ Ex003/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 42: Напишите программу, которая будет преобразовывать 
+//десятичное число в двоичное.
+
+
+void GetResult (int number)
+{
+    string result= "";
+    while (number > 0)
+    {
+        result = number%2 + result;
+        number /= 2;
+
+    }
+    Console.WriteLine(result); 
+}
+
+GetResult(2);
