43f9fb8929f920961dfc4820b952362a575ec054
Inna Osipova <rutheva_inna@mail.ru>
2022-07-14 12:45:44 +0300

    Числа Фибоначи

---
+++ Ex007/Program.cs
@@ -0,0 +1,14 @@
+﻿// F(1) = 1
+// f(2) =1
+// f(n) = f(n-1)+f(n-2);
+
+double Fibonnachi (int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return (Fibonnachi(n - 1) + Fibonnachi (n - 2));
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine(Fibonnachi(i));
+}
